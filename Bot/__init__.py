6f5f258e489ed1a1ded0cf7f8f56418b5ad17ea4
Primodial002 <115640591+Primodial002@users.noreply.github.com>
2022-12-02 12:19:09 +0530

    update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -11,13 +11,13 @@ LOG.setLevel(level=logging.INFO)
 #<-----------Variables-------------->
 LOG.info('❤️ Checking Bot Variables.....')
 TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '5518670370:AAHY8uARcJJe6pwEj5zkD9EFb8h_sKvttO0') #BOT Token Add
-API_ID = int(os.environ.get('API_ID', 16668169)) #Telgram Api id
-APP_HASH = os.environ.get('APP_HASH', '73f207e5b336836d4ffa9e1a2a7dde67')# Telgram App hash  
-OWNER_ID = int(os.environ.get('OWNER_ID', 1636310615))
-MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://SoloLevling7seven:solo7@cluster0.4nolwkk.mongodb.net/?retryWrites=true&w=majority') #MONGO DB FOR ANIME DATA
-FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1001622852308)
-BOT_NAME = os.environ.get('BOT_NAME', 'erika7701')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '5408040751:AAH3EMTJlBap6-t721xvUPpvQHXvu9-xMn0') #BOT Token Add
+API_ID = int(os.environ.get('API_ID', 15682957)) #Telgram Api id
+APP_HASH = os.environ.get('APP_HASH', '00b8b3714cee0ba2941091b7cc5578e7')# Telgram App hash  
+OWNER_ID = int(os.environ.get('OWNER_ID', 2090127712))
+MONGO_DB = 'mongodb+srv://gamma001:gamma001@cluster1.zhxrj.mongodb.net/?retryWrites=true&w=majority'
+FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1001746042894)
+BOT_NAME = os.environ.get('BOT_NAME', 'CompressorPro_bot')
 #<---------------Connecting-------------->
 if BOT_TOKEN is not None:
     try:
